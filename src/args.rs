2d67a4548b61ed6eeaf6e7ef7a1fd822ca130d92
Łukasz Biel <lukasz.biel@anixe.pl>
2019-10-09 00:47:03 +0200

    Update help to better explain what bts does

--- src/args.rs
+++ src/args.rs
@@ -7,8 +7,10 @@ const TEMPLATE_FOLDER_NAME: &str = ".bts";
 
 #[derive(StructOpt)]
 #[structopt(name = "bts", about = "Automatic template file generator.")]
+/// Generate file snippets at will
 pub struct Args {
     #[structopt(env = "BT_HOME", default_value = Self::default_template_folder())]
+    /// Location of snippets storage, default path is `~/.bt/templates` for osx and linux and `%HOME%\bt\templates` for windows
     pub config_location: PathBuf,
     #[structopt(flatten)]
     pub command: Command,
@@ -16,27 +18,37 @@ pub struct Args {
 
 #[derive(StructOpt)]
 pub enum Command {
+    /// Instantiate a snippet
     New(NewArgs),
+    /// Create new snippet
     Register(RegisterArgs),
 }
 
 #[derive(StructOpt)]
 pub struct NewArgs {
     #[structopt(long, short)]
+    /// Spawn with whole path leading to given snippet
     pub with_parent: bool,
+    /// Path to snippet
     pub template_name: String,
+    /// Target dir, `pwd` by default
     pub target_path: Option<PathBuf>,
     #[structopt(long, short, default_value = "32")]
+    /// Max depth that should be copied
     pub max_depth: u8,
 }
 
 #[derive(StructOpt)]
 pub struct RegisterArgs {
+    /// New snippet name
     pub template_name: String,
+    /// Path to single file or directory from which contents new snippet should be created
     pub path: PathBuf,
     #[structopt(long, short)]
+    /// Don't delete previous snippet when creating new one, only append
     pub append: bool,
     #[structopt(long, short, default_value = "32")]
+    /// Max depth that should be copied
     pub max_depth: u8,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
+//! Command line utility to create simple file snippets available to be instantiated at any time in future
+//!
+//! It allows to *register* and **spawn** snippets at will
+//!
+//! # Installation
+//! `cargo install bts`
+//!
+//! # Usage
+//! ## NEW
+//! Instantiate copy of an existing snippet
+//!
+//! `bts new SOURCE [DESTINATION] [-w/--with-parent] [-m/--max-depth _]`
+//!
+//! ### **SOURCE**
+//! - is snippet name.
+//!
+//! It's good idea to remember that for the time being snippets are stored in directories.
+//! Therefore nesting is allowed and advised.
+//! For example,
+//! `config/mysql`, `config/psql`, `config/sqlite` are good examples of template names
+//! and
+//! `config_mysql`, `config_psql`, `config_sqlite` are, while correct, discouraged.
+//! But hey, these are your snippets. Consider it only to be an advice.
+//!
+//! ### **DESTINATION**
+//! - is target folder name
+//!
+//! Place where you want to instantiate a snippet. By default `pwd` is used.
+//!
+//! ### **WITH-PARENT**
+//! - defines whether folder should be spawned preserving snippet name
+//!
+//! This means that `bts basic/template01 -w` will spawn files in `./basic/template01/` instead of `.`
+//!
+//! ### **MAX-DEPTH**
+//! - how deep the copy should go
+//!
+//! Copies snippet only until given depth is reached in directory tree.
+//! For example,
+//! ```text
+//! basic/template01/
+//!                 |- file.txt
+//!                 |-/ subdir
+//!
+//! bts basic/template01 -m 1
+//! ```
+//! will produce only file.txt in current directory.
+//! This parameter accepts numbers in range 0..255, default value is 32.
+//!
+//! ## Register
+//! Create new snippet from files
+//!
+//! `bts register TEMPLATE_NAME SOURCE [-a/--append] [-m/--max-depth]`
+//!
+//! ### **TEMPLATE_NAME**
+//! - is snippet name
+//!
+//! It clears previous snippets stored at given namespace, so calling `bts basic .` will also remove `basic/template01`.
+//!
+//! ### **SOURCE**
+//! - is a path to snippet files
+//!
+//! When **SOURCE** is a file, this file will be stored at snippet namespace.
+//! When **SOURCE** is a directory, all contents of that directory will be stored at snippet namespace.
+//!
+//! ### **APPEND**
+//! - defines whether we want to append to existing snippet
+//!
+//! This allows to create snippet only from selected files in directory.
+//!
+//! ### **MAX-DEPTH**
+//! - defines how deep should `bts` search for files when creating a snippet.
+//!
+//! Accepts numbers between 0 and 255, default value is 32.
+//!
+//! # Contribution
+//! I may accept new features, but that will only happen if I can see that it's useful.
+//! It's better to create issue at github before attempting to implement something.
+//! Bug fixes are always welcome.
+//! You can look into `.travis.yml` for build steps, but tldr is that I will merge only features that pass
+//! `cargo clippy --all-targets --all-features -- -D warnings`
+//! and
+//! `cargo fmt --all -- --check`
+//!
+//! # TBD
+//! * test coverage
+//! * default snippets (sample rust projects etc.)
+//! * pass-through for selected applications (eg. `bts spawn cargo/bin` calls `cargo new --bin`
+
 mod args;
 mod error;
 mod utils;
